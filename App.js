67c5fdb4233f5994e8e7ceffa99714f4b23718c7
Android Studio <devandroid@ort>
2025-05-23 09:28:38 -0300

    Subir

--- App.js
+++ App.js
@@ -1,20 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, ImageBackground, Image} from 'react-native';
+import { StyleSheet, Text, View, ImageBackground, Image, Alert,} from 'react-native';
+import { TextInput } from 'react-native-web';
+import {TouchableOpacity} from 'react-native';
+import {Pressable} from 'react-native';
+import { useState } from 'react';
 const imgExterna = {uri: 'https://upload.wikimedia.org/wikipedia/commons/0/00/ObeliscoBA2015.2.jpg'}
 const imgPerfil = {uri: 'https://tmssl.akamaized.net/images/foto/galerie/lionel-messi-argentinien-2022-1698689902-120754.jpg'}
+const fondoNoche = {uri: 'https://static.vecteezy.com/system/resources/previews/016/722/420/large_2x/the-obelisk-buenos-aires-argentina-at-night-2022-free-photo.jpg'}
+
 export default function App() {
+  const [imageSource, setImageSource] = useState(imgExterna)
+  const cambiarFondo=()=>{
+    setImageSource(imageSource.uri==imgExterna.uri?fondoNoche:imgExterna)
+  }
   return (
     <View style={[
       styles.container,{flexDirection:'column',},
     ]}>   
       <View style={{flex:1}}>
       <ImageBackground
-      source={imgExterna}
+      source={imageSource}
       resizeMode='cover'
       style={styles.backgroundImage}>
          <Image style={styles.image} source={imgPerfil} />
          <Text style={styles.text}>LIONEL MESSI</Text>
          <Text style={styles.text2}>CEO</Text>
+         <TextInput style={styles.Input} placeholder='Mensaje'></TextInput>
+         <TouchableOpacity style={styles.boton} onPress={   ()=> Alert.alert("AAAA") }>
+          <Text>Contactar</Text>
+        </TouchableOpacity>
+        <Pressable style={styles.boton2} onPress={cambiarFondo}>
+          <Text>Ver perfil</Text>
+        </Pressable>
       </ImageBackground>
       <StatusBar style='light' />
     </View>
@@ -46,13 +63,31 @@ const styles = StyleSheet.create({
     marginLeft:'auto',
     marginRight:'auto',
 
-    fontSize:'40px'
+    fontSize:'3rem'
   },
   text2:{
     borderColor:'black',
     marginLeft:'auto',
     marginRight:'auto',
-    marginBottom:'45%',
-    fontSize:'30px'
-  }
+    marginBottom:'10%',
+    fontSize:'2rem'
+  },
+  Input:{
+    marginLeft:'auto',
+    marginRight:'auto',
+    marginBottom:'5%',
+    backgroundColor:'white',
+  },
+ boton:{
+  marginLeft:'auto',
+  marginRight:'auto',
+  marginBottom:'5%',
+  backgroundColor:'white',
+ },
+ boton2:{
+  marginLeft:'auto',
+  marginRight:'auto',
+  marginBottom:'55%',
+  backgroundColor:'white',
+ }
 });
